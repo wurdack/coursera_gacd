4d6048ae53331762dd493e3be60432155adc4005
Richard Wurdack <wurdack@hotmail.com>
2015-05-23 12:06:26 -0700

    Load the library you're actually using dummy.

--- run_analysis.R
+++ run_analysis.R
@@ -4,6 +4,7 @@
 ## Imports the UCI HAR Dataset in a more useable form, then produces a tidy data set that
 ## summarizes the averages of 'mean' and 'standart deviation' based features for each subject and activity.
 ##
+library(dplyr)
 source("import.R")
 
 tidy <- data %>% group_by(subject_id, label) %>% summarise_each(funs(mean), matches("std|mean"))
